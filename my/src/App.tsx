4011b5c040baa35bdfb5d5011653e307c8670277
Paddingtonix <108619947+Paddingtonix@users.noreply.github.com>
2024-03-22 16:37:47 +0700

    clear code

--- my/src/App.tsx
+++ my/src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 
 //pages
 import Main from './pages/main-page-refactor';
-import InputCmp from "./components/input-cmp/input-cmp";
 import {useState} from "react";
 
 
@@ -14,28 +13,6 @@ function App() {
 
   	return (
     	<>
-			<div style={{padding: 20, width: 300, display: "flex", flexDirection: "column", gap: "12px"}}>
-				<InputCmp
-					value={v}
-					onChange={(v) => setV(v)}
-					label={"Email"}
-					type={"email"}
-					rules={{
-						required: true,
-						pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-					}}
-				/>
-				<InputCmp
-					value={v}
-					onChange={(v) => setV(v)}
-					label={"Пароль"}
-					type={"password"}
-					rules={{
-						required: true,
-					}}
-				/>
-			</div>
-
 		<Main></Main>
 		{/* <Router>
 			<Routes>
