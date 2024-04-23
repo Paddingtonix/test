b6411d23f0897c2dbd3f28ddc1c2a3f30308b2e5
Paddingtonix <108619947+Paddingtonix@users.noreply.github.com>
2024-04-23 13:42:45 +0700

    change settings for prototype

--- my/src/App.tsx
+++ my/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Main from './pages/main-page-refactor';
+// import Main from './pages/main-page-refactor';
 import {Navigate, Route, Routes} from "react-router-dom";
 import LoadDataPage from "./pages/load-data-page/load-data-page";
 import {useInstanceInterceptors} from "./utils/api";
 import { useNotification } from "./components/base/notification/notification-provider";
-import {useAuth} from "./utils/AuthProvider";
-import React from "react";
+// import {useAuth} from "./utils/AuthProvider";
+// import React from "react";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
             <Example/>
             <Routes>
                 <Route path={Links.LoadData} element={<LoadDataPage/>}/>
-                <Route path={Links.Main} element={<PrivateRoute><Main/></PrivateRoute>}/>
+                {/* <Route path={Links.Main} element={<PrivateRoute><Main/></PrivateRoute>}/> */}
                 <Route path={Links.NotFound} element={<Navigate to={Links.LoadData}/>}/>
             </Routes>
         </>
@@ -31,13 +31,13 @@ export const Links = {
 }
 
 //Хок, делающий путь недоступным для неавторизованного пользователя
-const PrivateRoute = ({children}: {children: React.ReactElement}) => {
-    const {isAuth} = useAuth();
+// const PrivateRoute = ({children}: {children: React.ReactElement}) => {
+//     const {isAuth} = useAuth();
 
-    return (
-        isAuth ? children : <Navigate to={Links.LoadData}/>
-    )
-}
+//     return (
+//         isAuth ? children : <Navigate to={Links.LoadData}/>
+//     )
+// }
 
 const Example = () => {
 
